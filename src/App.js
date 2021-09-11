c7f6d2d7db03bf802332614412033cb232fd5424
jusola <juulain@gmail.com>
2021-09-11 17:55:03 +0300

    Add reset key

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import volumemeter from "volume-meter";
 
 function App() {
   const [volume, setVolume] = useState(0);
+  const [barVolume, setBarVolume] = useState(0);
   const [maxVolume, setMaxVolume] = useState(0);
-
-  const [rVolume, setRVolume] = useState(0);
 
   const timerRef = useRef();
   const intervalRef = useRef();
   const volumeRef = useRef(0);
-  const mvRef = useRef(0);
+  const barVolRef = useRef(0);
 
   const queryParams = new URLSearchParams(window.location.search);
   const volumeMultiplier = queryParams.get("v") || 1;
@@ -24,57 +23,75 @@ function App() {
   }, [volumeMultiplier]);
 
   useEffect(() => {
-    volumeRef.current = volume;
-    mvRef.current = maxVolume;
-    if (volume > rVolume) {
-      setRVolume(volume);
+    function startBarTimer() {
+      timerRef.current = setTimeout(() => {
+        intervalRef.current = setInterval(() => {
+          const fullDist = barVolume - volumeRef.current;
+          var distTop = barVolume - barVolRef.current;
+          if (distTop < 0.1) distTop = 0.1;
+          const tm = 4;
+          const delta =
+            -(1 / (tm * fullDist)) * distTop ** 2 + (1 / tm) * distTop;
+          console.log(delta);
+          if (barVolRef.current - volumeRef.current < 0.2) {
+            setBarVolume(volumeRef.current);
+            if (timerRef.current) clearTimeout(timerRef.current);
+            if (intervalRef.current) clearInterval(intervalRef.current);
+          } else {
+            setBarVolume(barVolRef.current - delta);
+          }
+        }, 5);
+      }, 2500);
     }
-    if (volume > maxVolume) {
-      setMaxVolume(volume);
+
+    volumeRef.current = volume;
+    barVolRef.current = barVolume;
+    if (volume > barVolume) {
       if (timerRef.current) {
         clearTimeout(timerRef.current);
       }
       if (intervalRef.current) {
         clearInterval(intervalRef.current);
       }
-      mvRef.current = volume;
-      const triggerMv = volume;
-      timerRef.current = setTimeout(() => {
-        intervalRef.current = setInterval(() => {
-          const fullDist = triggerMv - volumeRef.current;
-          var distTop = triggerMv - mvRef.current;
-          if (distTop < 0.1) distTop = 0.1;
-          console.log(triggerMv, mvRef.current, fullDist, distTop);
-          const tm = 4;
-          const delta =
-            -(1 / (tm * fullDist)) * distTop ** 2 + (1 / tm) * distTop;
-          console.log(delta);
-          if (fullDist - distTop < 0.2) {
-            setMaxVolume(volumeRef.current);
-          } else {
-            setMaxVolume(mvRef.current - delta);
-          }
-        }, 5);
-      }, 2500);
+      setBarVolume(volume);
+      barVolRef.current = volume;
+      startBarTimer();
     }
-  }, [volume, maxVolume, rVolume]);
+  }, [volume, barVolume]);
+
+  useEffect(() => {
+    if (volume > maxVolume) {
+      setMaxVolume(volume);
+    }
+  }, [maxVolume, volume]);
+
+  useEffect(() => {
+    function onKeyDown(e) {
+      console.log(e);
+      if (e.key === "r") setMaxVolume(0);
+    }
+
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, []);
 
   return (
     <div className="App">
       <p
         className="CenterText"
         style={{
-          color: `rgb(${Math.floor((rVolume / 100) * 255)}, ${Math.floor(
-            255 - (rVolume / 100) * 255
+          color: `rgb(${Math.floor((maxVolume / 100) * 255)}, ${Math.floor(
+            255 - (maxVolume / 100) * 255
           )}, 0)`,
           backgroundColor: `rgba(${Math.floor(
-            (rVolume / 100) * 50
-          )}, ${Math.floor(50 - (rVolume / 100) * 50)}, 0, 0.5)`,
+            (maxVolume / 100) * 50
+          )}, ${Math.floor(50 - (maxVolume / 100) * 50)}, 0, 0.5)`,
           borderRadius: "8px",
           minWidth: "2em",
         }}
       >
-        {Math.round(rVolume)}
+        {Math.round(maxVolume)}
       </p>
       <div className="Inner">
         <div
@@ -90,12 +107,12 @@ function App() {
         <div
           style={{
             height: "2px",
-            bottom: `${maxVolume}%`,
+            bottom: `${barVolume}%`,
             width: "100%",
             position: "relative",
             backgroundColor: `rgb(${Math.floor(
-              (maxVolume / 100) * 255
-            )}, ${Math.floor(255 - (maxVolume / 100) * 255)}, 0)`,
+              (barVolume / 100) * 255
+            )}, ${Math.floor(255 - (barVolume / 100) * 255)}, 0)`,
           }}
         ></div>
       </div>
